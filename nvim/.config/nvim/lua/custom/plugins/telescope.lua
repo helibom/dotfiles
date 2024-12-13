e2a793d320089a03187edd079802626511398170
Henrik Lindblom <helibom.contact@gmail.com>
2024-12-13 21:50:33 +0100

    wip(nvim): hack on telescope.nvim

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -2,8 +2,23 @@ return {
     {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	-- or                          , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{
+		    'nvim-telescope/telescope-fzf-native.nvim',
+		    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+		}
+	},
 	config = function ()
+	    require('telescope').setup {
+		-- pickers = {}
+		extensions = {
+		    fzf = {}
+		}
+	    }
+	    require('telescope').load_extension('fzf')
+
+
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
