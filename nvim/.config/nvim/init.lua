d9ab9e30f0dbe79c4d14787e607d5b84193f5a51
Henrik Lindblom <helibom.contact@gmail.com>
2025-02-13 14:40:19 +0100

    feat(nvim/config): use TJ DeVries multigrep telescope picker

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,4 +2,5 @@ require("config.lazy")
 require("config.lualine")
 require("config.utils.file-utils")
 require("custom.snippets.floatterm")
+require("config.telescope.multigrep").setup()
 
--- nvim/.config/nvim/lua/config/telescope/multigrep.lua
+++ nvim/.config/nvim/lua/config/telescope/multigrep.lua
@@ -15,41 +15,39 @@ local live_multigrep = function(opts)
         return nil
       end
 
-      local pieces = vim.split(prompt, "  ") -- double space
+      local pieces = vim.split(prompt, "  ")
       local args = { "rg" }
-      local arg_opts = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" }
-      if pieces[1] and #pieces < 2 then
+      if pieces[1] then
         table.insert(args, "-e")
-	vim.list_extend(args, arg_opts)
         table.insert(args, pieces[1])
       end
 
-      if pieces[1] and pieces[2] and #pieces == 2 then
+      if pieces[2] then
         table.insert(args, "-g")
-	vim.list_extend(args, arg_opts)
-        table.insert(args, pieces[1])
         table.insert(args, pieces[2])
       end
 
       ---@diagnostic disable-next-line: deprecated
-      vim.notify(vim.inspect(args), vim.log.levels.DEBUG)
-      return args
-  end,
-  entry_maker = make_entry.gen_from_vimgrep(opts),
-  cwd = opts.cwd,
+      return vim.tbl_flatten {
+        args,
+        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+      }
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
   }
 
   pickers.new(opts, {
-      debounce = 100,
-      prompt_title = "Multi Grep",
-      finder = finder,
-      previewer = conf.grep_previewer(opts),
-      sorter = require("telescope.sorters").empty(),
+    debounce = 100,
+    prompt_title = "Multi Grep",
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty(),
   }):find()
 end
 
 M.setup = function()
-    vim.keymap.set("n", "<leader>fg", live_multigrep)
+  vim.keymap.set("n", "<leader>fm", live_multigrep)
 end
 
 return M
