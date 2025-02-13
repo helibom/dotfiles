28b01dfb57d5fc1549eeb3b3ea5c1e7ff06638bd
Henrik Lindblom <helibom.contact@gmail.com>
2025-02-13 14:38:58 +0100

    feat(nvim/telescope): add git pickers and old files picker to keybindings

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -21,12 +21,16 @@ return {
 
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	    vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope recently open files' })
 	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 	    vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { desc = 'Telescope fuzzy find current buffer' })
 	    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 	    vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = 'Telescope register' })
 	    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope normal mode keymaps' })
+	    vim.keymap.set('n', '<leader>fcc', builtin.git_commits, { desc = 'Telescope repository commits' })
+	    vim.keymap.set('n', '<leader>fcb', builtin.git_bcommits, { desc = 'Telescope buffer commits' })
+	    vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
 	    vim.keymap.set(
 	    	'n',
 	    	'<leader>ec',
