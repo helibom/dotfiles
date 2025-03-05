54ea15e2a7b58df80e9d2e1728dd4bfc21ab5987
Henrik Lindblom <helibom.contact@gmail.com>
2025-03-05 15:11:45 +0100

    feat(nvim/snippets/autocmds): autocmd for deleting list items from qflist

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,5 +2,6 @@ require("config.lazy")
 require("config.lualine")
 require("config.utils.file-utils")
 require("custom.snippets.floatterm")
+require("custom.snippets.autocmds")
 require("config.telescope.multigrep").setup()
 
