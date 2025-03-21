f0db5c63f99f04ca6a60bd6c5e9252e87b6f59cc
Henrik Lindblom <helibom.contact@gmail.com>
2025-03-21 09:21:28 +0100

    feat(nvim/formatting): bind "=C" to format with conform.nvim; use 'zprint' formatter for Clojure

--- nvim/.config/nvim/lua/custom/plugins/formatting.lua
+++ nvim/.config/nvim/lua/custom/plugins/formatting.lua
@@ -3,7 +3,8 @@ return {
 	'stevearc/conform.nvim',
 	opts = {},
 	config = function ()
-	    require("conform").setup {
+	    local conform = require("conform")
+	    conform.setup {
 		formatters_by_ft = {
 		    json = { "jq" },
 		    yaml = { "yamlfmt" },
@@ -12,10 +13,19 @@ return {
 		    javascript = { "prettier" },
 		    javascriptreact = { "prettier" },
 		    typescriptreact = { "prettier" },
-		    clojure = { "cljfmt" },
+		    clojure = { "cljfmt", "zprint" },
 		    htmldjango = { "djlint" },
 		}
 	    }
+	    vim.keymap.set("n", "=C", function ()
+		conform.format({}, function (err, did_edit)
+		    if err then
+			vim.notify("Error: " .. err, vim.log.levels.ERROR)
+		    elseif did_edit then
+			vim.cmd('echo "Conform(atted)"')
+		    end
+		end)
+	    end, { desc = "Format with conform.nvim" })
 	end
     }
 }
